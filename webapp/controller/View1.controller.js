9a16f176876282f7d59b8ee1680c0a2c80667ffb
haritha2528 <devipinni21@gmail.com>
2020-11-11 09:49:44 +0000

    Initial Commit

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,5 +1,6 @@
 sap.ui.define([
-		"sap/ui/core/mvc/Controller"
+        "sap/ui/core/mvc/Controller",
+        "sap/ui/model/json/JSONModel"
 	],
 	/**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
@@ -9,6 +10,14 @@ sap.ui.define([
 
 		return Controller.extend("ns.Project1.controller.View1", {
 			onInit: function () {
+                var sServiceUrl = "/sap/opu/odata/sap/EPM_REF_APPS_PROD_MAN_SRV/Products?$format=json";
+                var oModel = new sap.ui.model.odata.ODataModel(sServiceUrl);
+                // @ts-ignore
+                this.getView().setModel(oModel);
+                //crm model
+                 var oCRMModel = new sap.ui.model.json.JSONModel("/sap/opu/odata/sap/EPM_REF_APPS_PROD_MAN_SRV/Products?$format=json");
+                 // @ts-ignore
+                 this.getView().byId("crmcontainer").setModel(oCRMModel, "oCRMModel");
 
 			}
 		});
